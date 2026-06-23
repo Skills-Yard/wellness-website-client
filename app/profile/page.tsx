c5b4071f6aedb94c290abbc9687643605c55e363
Himanshu Mandowra <himanshu.toppr@gmail.com>
2026-06-23 14:59:13 +0530

    feat: Update Detail component to utilize full screen height for improved layout

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,7 +2,7 @@ import ProfilePage from "@/components/profile/Profile";
 
 export default function Detail() {
     return(
-        <div className="bg-white">
+        <div className="bg-white h-screen ">
 
             <ProfilePage/>
         </div>
